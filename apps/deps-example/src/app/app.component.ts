0593e9b5f750f941960c472cf62484ee70567fe0
gka <gkamperis@msn.com>
2023-06-21 19:46:31 +0100

    Initial commit

---
+++ apps/deps-example/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'deps-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'deps-example';
+}
